870682b195d0acfa719e360c608f5083f032e4eb
Dachulya <111542022+Dachulya@users.noreply.github.com>
2023-08-31 16:26:57 +0300

    of commit

--- src/main/java/DAO/EmployeeDAO.java
+++ src/main/java/DAO/EmployeeDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 public interface EmployeeDAO {
 
 
+
         List<Employee> getAllEmployee();
         EmployeeDAO getEmployeeById(int id);
         void createEmployee(Employee employee);
